ba3182d1f1fe30e94f8ab40bb9c43b72287169f8
Marcello Annicchiarico <marcello.annicchiarico@eva.global>
2023-09-11 23:52:04 +0200

    add vercel/analytics

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -2,6 +2,11 @@
   import Navbar from "$lib/Navbar.svelte";
   import Footer from "$lib/Footer.svelte";
   import "../app.css";
+
+  import { dev } from "$app/environment";
+  import { inject } from "@vercel/analytics";
+  inject({ mode: dev ? "development" : "production" });
+
   // @ts-ignore
   import AOS from "aos";
   import "aos/dist/aos.css";
