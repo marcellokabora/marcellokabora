49acb4f73db446939fc152d3e040f8788c73ba91
Marcello Kabora <marcellokabora@gmail.com>
2024-02-01 11:20:27 +0100

    wip

--- src/routes/project/eva-heatmap/+page.svelte
+++ src/routes/project/eva-heatmap/+page.svelte
@@ -25,10 +25,10 @@
 
 <ProjectUi {data}>
   <p>
-    EVA Heatmap is a platform for evaluate the status and performance of the
-    chargers and evses in real time and on specific location in the map. This
-    design show how to navigate from the search to a specific location and
-    identify specific chargers and evses with the help of the heatmap is
-    possible to individuate what area are in down or up status.
+    EVA Heatmap is a platform for evaluate the status and performance of
+    chargers and evses in real time in specific location in the map. This design
+    show how to navigate from the search to a specific location and identify the
+    chargers and evses with down or up status reflecting the red and green color
+    from the google map heatmap.
   </p>
 </ProjectUi>
