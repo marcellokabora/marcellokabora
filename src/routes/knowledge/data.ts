55c2359bf2b71075bad46f4107b898b68a59237f
Marcello Kabora <marcellokabora@gmail.com>
2025-02-17 13:40:27 +0100

    sticky

---
+++ src/routes/knowledge/data.ts
@@ -0,0 +1,312 @@
+export const menu = [
+  "javascript",
+  "design",
+  "database",
+  "software",
+  "management",
+];
+
+const javascript = [
+  {
+    id: "javascript",
+    name: "nodejs",
+    title: "Nodejs",
+    photo: "/photo/nodejs.png",
+    link: "//nodejs.org",
+  },
+  {
+    id: "javascript",
+    name: "svelte",
+    title: "Svelte",
+    photo: "/photo/svelte.png",
+    link: "//svelte.dev",
+  },
+  {
+    name: "angular",
+    title: "Angular",
+    photo: "/photo/angular.png",
+    link: "//angular.io",
+  },
+  {
+    name: "nx",
+    title: "NX",
+    photo: "/logo/nx.png",
+    link: "//nx.dev",
+  },
+  {
+    name: "vue",
+    title: "Vue",
+    photo: "/photo/vue.svg",
+    link: "//vuejs.org",
+  },
+  {
+    name: "react",
+    title: "React",
+    photo: "/photo/react.png",
+    link: "//reactjs.org",
+  },
+  {
+    name: "next",
+    title: "Next",
+    photo: "/photo/next.png",
+    link: "//nextjs.org",
+  },
+  {
+    name: "jquery",
+    title: "jQuery",
+    photo: "/photo/jquery.webp",
+    link: "//jquery.com",
+  },
+  {
+    name: "formly",
+    title: "Formly",
+    photo: "/photo/formly.png",
+    link: "//formly.dev",
+  },
+  {
+    name: "ckeditor",
+    title: "Ckeditor",
+    photo: "/photo/ckeditor.png",
+    link: "//ckeditor.com",
+  },
+  {
+    name: "d3",
+    title: "D3",
+    photo: "/photo/d3.png",
+    link: "//d3js.org",
+  },
+  {
+    name: "highchart",
+    title: "Highchart",
+    photo: "/photo/highchart.png",
+    link: "//highcharts.com",
+  },
+  {
+    name: "pwa",
+    title: "PWA",
+    photo: "/photo/pwa.png",
+    link: "//web.dev/progressive-web-apps/",
+  },
+];
+
+const design = [
+  {
+    id: "design",
+    name: "figma",
+    title: "Figma",
+    photo: "/photo/figma.svg",
+    link: "//figma.com",
+  },
+  {
+    name: "miro",
+    title: "Miro",
+    photo: "/photo/miro.png",
+    link: "//miro.com",
+  },
+  {
+    name: "abstract",
+    title: "Abstract",
+    photo: "/photo/abstract.png",
+    link: "//abstract.com",
+  },
+  {
+    name: "photoshop",
+    title: "Photoshop",
+    photo: "/photo/photoshop.png",
+    link: "//photoshop.com",
+  },
+  {
+    name: "illustrator",
+    title: "Illustrator",
+    photo: "/photo/illustrator.png",
+    link: "//adobe.com/illustrator",
+  },
+  {
+    name: "storybook",
+    title: "Storybook",
+    photo: "/photo/storybook.png",
+    link: "//storybook.js.org",
+  },
+  {
+    name: "material",
+    title: "Material",
+    photo: "/photo/material.png",
+    link: "//material.angular.io",
+  },
+  {
+    name: "bootstrap",
+    title: "Bootstrap",
+    photo: "/photo/bootstrap.png",
+    link: "//getbootstrap.com",
+  },
+  {
+    name: "tinkercad",
+    title: "Tinkercad",
+    photo: "/photo/tinkercad.webp",
+    link: "//tinkercad.com",
+  },
+];
+
+const database = [
+  {
+    id: "database",
+    name: "git",
+    title: "GIT",
+    photo: "/photo/git.png",
+    link: "//git-scm.com",
+  },
+  {
+    name: "npm",
+    title: "NPM",
+    photo: "/photo/npm.png",
+    link: "//npmjs.com",
+  },
+  {
+    name: "firebase",
+    title: "Firebase",
+    photo: "/photo/firebase.png",
+    link: "//firebase.google.com",
+  },
+  {
+    name: "supabase",
+    title: "Supabase",
+    photo: "/photo/supabase.jpg",
+    link: "//supabase.com",
+  },
+  {
+    name: "php",
+    title: "PHP",
+    photo: "/photo/php.svg",
+    link: "//php.net",
+  },
+  {
+    name: "mysql",
+    title: "MySQL",
+    photo: "/photo/mysql.png",
+    link: "//mysql.com",
+  },
+  {
+    name: "wordpress",
+    title: "Wordpress",
+    photo: "/photo/wordpress.png",
+    link: "//wordpress.com",
+  },
+  {
+    name: "webflow",
+    title: "Web Flow",
+    photo: "/photo/webflow.png",
+    link: "//webflow.com",
+  },
+  {
+    name: "builder",
+    title: "Builder",
+    photo: "/photo/builder.jpeg",
+    link: "//www.builder.io/",
+  },
+];
+
+const software = [
+  {
+    id: "software",
+    name: "firefox",
+    title: "Firefox",
+    photo: "/photo/firefox.png",
+    link: "//mozilla.org",
+  },
+  {
+    name: "chrome",
+    title: "Chrome",
+    photo: "/photo/chrome.png",
+    link: "//google.com/chrome",
+  },
+  {
+    name: "safari",
+    title: "Safari",
+    photo: "/photo/safari.png",
+    link: "//apple.com/safari",
+  },
+  {
+    name: "edge",
+    title: "Edge",
+    photo: "/photo/edge.png",
+    link: "//microsoft.com/en-us/edge",
+  },
+  {
+    name: "vsc",
+    title: "Visual Studio Code",
+    photo: "/photo/vsc.png",
+    link: "//visualstudio.com",
+  },
+  {
+    name: "brackets",
+    title: "Brackets",
+    photo: "/photo/brackets.png",
+    link: "//brackets.io",
+  },
+  {
+    name: "slack",
+    title: "Slack",
+    photo: "/photo/slack.png",
+    link: "//slack.com",
+  },
+  {
+    name: "ableton",
+    title: "Ableton",
+    photo: "/photo/ableton.png",
+    link: "//ableton.com",
+  },
+  {
+    name: "1password",
+    title: "1Password",
+    photo: "/photo/1password.png",
+    link: "//1password.com",
+  },
+];
+
+const management = [
+  {
+    id: "management",
+    name: "github",
+    title: "Github",
+    photo: "/photo/github.png",
+    link: "//github.com",
+  },
+  {
+    name: "gitlab",
+    title: "Gitlab",
+    photo: "/photo/gitlab.png",
+    link: "//gitlab.com",
+  },
+  {
+    name: "jira",
+    title: "Jira",
+    photo: "/photo/jira.png",
+    link: "//atlassian.com/jira",
+  },
+  {
+    name: "azure",
+    title: "Azure",
+    photo: "/photo/azure.webp",
+    link: "//azure.microsoft.com",
+  },
+  {
+    name: "asana",
+    title: "Asana",
+    photo: "/photo/asana.png",
+    link: "//asana.com",
+  },
+  {
+    name: "vercel",
+    title: "Vercel",
+    photo: "/logo/vercel.svg",
+    link: "//vercel.com",
+  },
+];
+
+export const items = [
+  ...javascript,
+  ...design,
+  ...database,
+  ...software,
+  ...management,
+];
