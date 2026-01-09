34441bbe354a0961d1437d2607bf3a3d622c3302
Marcello Annicchiarico <marcellokabora@gmail.com>
2026-01-09 16:56:31 +0100

    wip

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -19,8 +19,32 @@
     name="keywords"
     content="Software, Developer, Frontend, Fullstack, Developer, UI/UX, Nodejs, Svelte, Angular, Vue, React, Material, Firebase"
   />
+
+  <!-- Open Graph / LinkedIn Meta Tags -->
+  <meta property="og:type" content="article" />
+  <meta property="og:url" content="https://marcellokabora.com" />
+  <meta
+    property="og:title"
+    content="Marcello Annicchiarico | Fronted AI Developer"
+  />
+  <meta
+    property="og:description"
+    content="Senior Frontend Developer with a decade of experience in software development, UI/UX, and data visualization. Passionate about building high-performance UI libraries and PWAs for SaaS and B2B, using frameworks like Vue, Svelte, Angular, and React."
+  />
+  <meta
+    property="og:image"
+    content="https://marcellokabora.com/gallery/keyboard.jpg"
+  />
+  <meta property="og:image:width" content="1200" />
+  <meta property="og:image:height" content="630" />
+
+  <!-- Article Meta Tags -->
+  <meta property="article:author" content="Marcello Annicchiarico" />
+  <meta property="article:published_time" content="2024-01-01T00:00:00Z" />
+  <meta property="article:modified_time" content="2026-01-09T00:00:00Z" />
+
+  <!-- Legacy thumbnail -->
   <meta name="thumbnail" content="/gallery/keyboard.jpg" />
-  <meta property="og:image" content="/gallery/keyboard.jpg" />
 </svelte:head>
 
 <main
