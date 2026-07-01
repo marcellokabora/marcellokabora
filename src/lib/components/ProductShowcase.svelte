7fb777b6ceff595ec8cc127c39d37642a97a22f7
Marcello Annicchiarico <marcellokabora@gmail.com>
2026-07-01 22:24:18 +0200

    wip

--- src/lib/components/ProductShowcase.svelte
+++ src/lib/components/ProductShowcase.svelte
@@ -113,23 +113,6 @@
                   {project.slogan}
                 </p>
               {/if}
-              <div
-                class="flex items-center gap-2 text-white font-semibold text-sm opacity-0 group-hover:opacity-100 transition-opacity duration-300"
-              >
-                View Project
-                <svg
-                  class="w-4 h-4 group-hover:translate-x-1 transition-transform"
-                  fill="none"
-                  stroke="currentColor"
-                  viewBox="0 0 24 24"
-                  ><path
-                    stroke-linecap="round"
-                    stroke-linejoin="round"
-                    stroke-width="2"
-                    d="M17 8l4 4m0 0l-4 4m4-4H3"
-                  ></path></svg
-                >
-              </div>
             </div>
           </div>
         </a>
